1f5ff954ef3dc64b0762b255496e9e4eb67750f0
ProfMara <94469709+ProfMara@users.noreply.github.com>
2023-07-10 15:02:07 -0300

    Add files via upload

---
+++ code1.py
@@ -0,0 +1,47 @@
+import cv2
+import mediapipe as mp
+cap = cv2.VideoCapture(0)
+#mãos
+mp_hands = mp.solutions.hands
+#ferramentas de desenho
+mp_draw = mp.solutions.drawing_utils
+
+hands = mp_hands.Hands(min_detection_confidence=0.6, min_tracking_confidence=0.5 )
+tipIds = [4,8,12,16,20]
+
+def contarDedos(image, hand_landmarks, handNo=0):
+    
+    fingers = []
+    if hand_landmarks:
+        print("contar dedos")
+        landmarks = hand_landmarks[handNo].landmark
+        for i in tipIds:
+            finger_tip_y = landmarks[i].y
+            finger_bottom_y = landmarks[i-2].y
+            if i != 4:
+                if finger_tip_y < finger_bottom_y:
+                    fingers.append(1)
+                else:
+                    fingers.append(0)
+            total = fingers.count(1)
+            texto = f"Dedos: {total}"
+            cv2.putText(image, texto, (50,50),cv2.FONT_HERSHEY_COMPLEX, 1, (255,0,0), 2 )
+
+def desenharMarcas(image, marcas):
+    if marcas:
+        for marca in marcas:
+            mp_draw.draw_landmarks(image, marca, mp_hands.HAND_CONNECTIONS)
+
+
+while True:
+    success, image = cap.read()
+    if success:
+        results = hands.process(image)
+        desenharMarcas(image, results.multi_hand_landmarks )
+        contarDedos(image,results.multi_hand_landmarks )
+        cv2.imshow("", image)
+
+    if cv2.waitKey(1)==32:
+        break
+
+
